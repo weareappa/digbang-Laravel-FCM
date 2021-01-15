44a2bf811985c789b538a9afb564ec8fad0b9bb4
Francisco Suarez <fsuarez@digbang.com>
2021-01-15 15:05:49 -0300

    ServiceProvider fixed

--- src/FCMServiceProvider.php
+++ src/FCMServiceProvider.php
@@ -16,31 +16,33 @@ class FCMServiceProvider extends ServiceProvider
         if (Str::contains($this->app->version(), 'Lumen')) {
             $this->app->configure('fcm');
         } else {
-            $this->publishes([
-                __DIR__.'/../config/fcm.php' => config_path('fcm.php'),
-            ]);
+            $this->publishes(
+                [
+                    __DIR__ . '/../config/fcm.php' => config_path('fcm.php'),
+                ]
+            );
         }
     }
 
     public function register()
     {
-		if (!Str::contains($this->app->version(), 'Lumen')) {
+        if (!Str::contains($this->app->version(), 'Lumen')) {
             $this->mergeConfigFrom(__DIR__.'/../config/fcm.php', 'fcm');
         }
 
-        $this->app->singleton('fcm.client', function ($app) {
+        $this->app->singleton(FCMManager::class, function ($app) {
             return (new FCMManager($app))->driver();
         });
 
-        $this->app->bind('fcm.group', function ($app) {
-            $client = $app[ 'fcm.client' ];
+        $this->app->bind(FCMGroup::class, function ($app) {
+            $client = $app[FCMManager::class];
             $url = $app[ 'config' ]->get('fcm.http.server_group_url');
 
             return new FCMGroup($client, $url);
         });
 
-        $this->app->bind('fcm.sender', function ($app) {
-            $client = $app[ 'fcm.client' ];
+        $this->app->bind(FCMSender::class, function ($app) {
+            $client = $app[FCMManager::class];
             $url = $app[ 'config' ]->get('fcm.http.server_send_url');
 
             return new FCMSender($client, $url);
@@ -49,6 +51,6 @@ class FCMServiceProvider extends ServiceProvider
 
     public function provides()
     {
-        return ['fcm.client', 'fcm.group', 'fcm.sender'];
+        return [FCMManager::class, FCMGroup::class, FCMSender::class];
     }
 }
